0142ae143911bc7f61140384bbe7ffcf123f8599
Cidiomar Dias <cidiomar.jr@gmail.com>
2020-01-14 17:51:18 -0200

    Merge code from metabase v0.34.1

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject metabase/bigquery-alt-driver "0.34.0-SNAPSHOT-1.27.0"
+(defproject metabase/bigquery-alt-driver "0.34.0-SNAPSHOT-1.30.3"
   :min-lein-version "2.5.0"
 
   :aliases
   {"install-for-building-drivers" ["with-profile" "+install-for-building-drivers" "install"]}
 
   :dependencies
-  [[com.google.apis/google-api-services-bigquery "v2-rev20181202-1.27.0"]]
+  [[com.google.apis/google-api-services-bigquery "v2-rev20190917-1.30.3"]]
 
   :profiles
   {:provided
    {:dependencies
     [[org.clojure/clojure "1.10.1"]
      [metabase-core "1.0.0-SNAPSHOT"]
-     [metabase/google-driver "1.0.0-SNAPSHOT-1.27.0"]]}
+     [metabase/google-driver "1.0.0-SNAPSHOT-1.30.7"]]}
 
    :install-for-building-drivers
    {:auto-clean true
